c0a307847b7538a866257992120fa663bc563571
HueTran <trth.thanhue@gmail.com>
2025-06-21 20:45:15 +0700

    feat: add Order Management section with titles and placeholders in English and Vietnamese translations

--- i18n/request.ts
+++ i18n/request.ts
@@ -8,7 +8,7 @@ export default getRequestConfig(async ({ locale }) => {
   }
 
   return {
-    locale,
+    locale: locale as string,
     messages: (await import(`../messages/${locale}.json`)).default
   };
 });

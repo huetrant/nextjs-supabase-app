9400ee5606ce80003dc13cd7c54a5eccaf03c0a8
HueTran <trth.thanhue@gmail.com>
2025-06-21 18:48:21 +0700

    feat: add next-intl plugin integration and update configuration files

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin('./i18n.ts');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
+};
+
+module.exports = withNextIntl(nextConfig);
